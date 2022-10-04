5399241d214d98be6a6c64873e73a34229b5ce54
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-10-04 22:58:39 +0100

    First few test cases

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,5 +1,15 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
+	username = models.CharField(
+		max_length=30,
+	 	unique=True,
+	 	validators=[RegexValidator(
+	 			regex=r'^@\w{3,}$',
+	 			message='Username must consist of @ followed by at least three alphanumericals'
+	 		)]
+	 	)
 	bio = models.TextField()
