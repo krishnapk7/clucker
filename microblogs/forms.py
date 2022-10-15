ced73db51e13d435960664c3845105bc5e40fbc6
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-10-15 16:47:17 +0100

    Add Sign up Form

---
+++ microblogs/forms.py
@@ -0,0 +1,13 @@
+from cProfile import label
+from django import forms
+from django import forms
+from .models import User
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'bio']
+        widgets = {'bio': forms.Textarea()}
+
+    new_password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
