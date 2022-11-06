342cff7a4da0eecb624149a25447ca37a361e8e1
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-11-06 16:36:03 +0000

    Render sign up form with bootstrap

--- clucker/settings.py
+++ clucker/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'microblogs'
+    'microblogs',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
